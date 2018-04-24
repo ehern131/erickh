44b3bafc1a2b12274995450ff00720158f866107
Erick Hernandez <ehern131@gmail.com>
2018-04-24 00:24:39 -0400

    Testing Landing pages & adding routes

--- server.rb
+++ server.rb
@@ -3,6 +3,15 @@ require 'sinatra'
 get '/' do
   File.read(File.join('public','index.html'))
 end
+get '/index' do
+  File.read(File.join('public','index.html'))
+end
 get '/portfolio-classic' do
   File.read(File.join('public','portfolio-classic.html'))
+end
+get '/about-bigbold' do
+  File.read(File.join('public','about-bigbold.html'))
+end
+get '/myversion' do
+  File.read(File.join('public','myversion.html'))
 end
