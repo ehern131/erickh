62bdc36e4bf4c3e06b888130fe895bdfe1082edb
Erick Hernandez <ehern131@gmail.com>
2018-12-12 13:34:18 -0500

    Add htaccess

--- server.rb
+++ server.rb
@@ -3,54 +3,54 @@ require 'sinatra'
 
 # WORK INDEX
 get '/' do
-  File.read(File.join('public','construction.html'))
+  File.read(File.join('public','construction'))
 end
 get '/index' do
-  File.read(File.join('public','index.html'))
+  File.read(File.join('public','index'))
 end
 get '/portfolio' do
-  File.read(File.join('public','index.html'))
+  File.read(File.join('public','index'))
 end
 get '/work' do
-  File.read(File.join('public','index.html'))
+  File.read(File.join('public','index'))
 end
 
 
 # MOLLIE FINANCIAL LEASE MANAGEMENT SYSTEM (LMS)
 get '/molliefinancial' do
-  File.read(File.join('public','molliefinancial.html'))
+  File.read(File.join('public','molliefinancial'))
 end
 get '/mollie' do
-  File.read(File.join('public','molliefinancial.html'))
+  File.read(File.join('public','molliefinancial'))
 end
 
 
 # SKY FINANCIAL MERCHANT PORTAL (MP)
 get '/skyfinancial' do
-  File.read(File.join('public','skyfinancial.html'))
+  File.read(File.join('public','skyfinancial'))
 end
 get '/skyfinancial2' do
-  File.read(File.join('public','skyfinancial2.html'))
+  File.read(File.join('public','skyfinancial2'))
 end
 
 
 # QUALITY CRUISES
 get '/qualitycruises' do
-  File.read(File.join('public','qualitycruises.html'))
+  File.read(File.join('public','qualitycruises'))
 end
 # QUALITY CRUISES
 get '/qualitycruises2' do
-  File.read(File.join('public','qualitycruises2.html'))
+  File.read(File.join('public','qualitycruises2'))
 end
 
 
 # TEST PAGES (FUTURE TEMPLATE)
 get '/portfolio-classic' do
-  File.read(File.join('public','portfolio-classic.html'))
+  File.read(File.join('public','portfolio-classic'))
 end
 get '/about-bigbold' do
-  File.read(File.join('public','about-bigbold.html'))
+  File.read(File.join('public','about-bigbold'))
 end
 get '/myversion' do
-  File.read(File.join('public','myversion.html'))
+  File.read(File.join('public','myversion'))
 end
