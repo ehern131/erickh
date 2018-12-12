359a9e2007c76db50b4ae9960d099f5fbff382c7
Erick Hernandez <ehern131@gmail.com>
2018-12-12 13:36:57 -0500

    Add HTML to routes

--- server.rb
+++ server.rb
@@ -3,54 +3,54 @@ require 'sinatra'
 
 # WORK INDEX
 get '/' do
-  File.read(File.join('public','construction'))
+  File.read(File.join('public','construction.html'))
 end
 get '/index' do
-  File.read(File.join('public','index'))
+  File.read(File.join('public','index.html'))
 end
 get '/portfolio' do
-  File.read(File.join('public','index'))
+  File.read(File.join('public','index.html'))
 end
 get '/work' do
-  File.read(File.join('public','index'))
+  File.read(File.join('public','index.html'))
 end
 
 
 # MOLLIE FINANCIAL LEASE MANAGEMENT SYSTEM (LMS)
 get '/molliefinancial' do
-  File.read(File.join('public','molliefinancial'))
+  File.read(File.join('public','molliefinancial.html'))
 end
 get '/mollie' do
-  File.read(File.join('public','molliefinancial'))
+  File.read(File.join('public','molliefinancial.html'))
 end
 
 
 # SKY FINANCIAL MERCHANT PORTAL (MP)
 get '/skyfinancial' do
-  File.read(File.join('public','skyfinancial'))
+  File.read(File.join('public','skyfinancial.html'))
 end
 get '/skyfinancial2' do
-  File.read(File.join('public','skyfinancial2'))
+  File.read(File.join('public','skyfinancial2.html'))
 end
 
 
 # QUALITY CRUISES
 get '/qualitycruises' do
-  File.read(File.join('public','qualitycruises'))
+  File.read(File.join('public','qualitycruises.html'))
 end
 # QUALITY CRUISES
 get '/qualitycruises2' do
-  File.read(File.join('public','qualitycruises2'))
+  File.read(File.join('public','qualitycruises2.html'))
 end
 
 
 # TEST PAGES (FUTURE TEMPLATE)
 get '/portfolio-classic' do
-  File.read(File.join('public','portfolio-classic'))
+  File.read(File.join('public','portfolio-classic.html'))
 end
 get '/about-bigbold' do
-  File.read(File.join('public','about-bigbold'))
+  File.read(File.join('public','about-bigbold.html'))
 end
 get '/myversion' do
-  File.read(File.join('public','myversion'))
+  File.read(File.join('public','myversion.html'))
 end
